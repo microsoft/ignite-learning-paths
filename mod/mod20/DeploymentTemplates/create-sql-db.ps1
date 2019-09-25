4f6af4678bd045d55480a9e1aaa33c3ade30f71c
Neil Peterson <neilpeterson@outlook.com>
2019-09-25 15:57:11 +0100

    Adding SQL DB creation script + extension

---
+++ mod/mod20/DeploymentTemplates/create-sql-db.ps1
@@ -0,0 +1,6 @@
+$dbName = "tailwind"
+
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+$server = new-object ("Microsoft.SqlServer.Management.Smo.Server")
+$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -argumentlist $server, $dbName
+$db.Create()
