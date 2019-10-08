4e7ce5abdfe935e257295e560f95878824160bac
Neil Peterson <neilpeterson@outlook.com>
2019-10-08 15:23:27 -0700

    PowerShell Module

--- ops/deployment/storage.ps1
+++ ops/deployment/storage.ps1
@@ -3,5 +3,7 @@ param (
     [string]$storageAccountName
 )
 
+Install-Module -Name Az -AllowClobber -Scope AllUsers
+
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $azureResourceGroup -Name $storageAccountName
 New-AzStorageTable –Name oncall –Context $storageAccount.Context
